479201bdbbd6b8937b93003c38a8714430e22f0a
Paweł Pietras <pawel.pietras144@gmail.com>
2024-09-26 12:08:08 +0200

    listing2-4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,30 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
-import { Osoba } from './interfaceOsoba';
+import { Osoba, Plec } from './interfaceOsoba';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule],
+  imports: [RouterOutlet, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'projekt-2';
   powitanie: string = 'Witaj';
   osoba: Osoba = {
     imie: 'Tadeusz',
     nazwisko: 'Nowak',
     zdjecie: 'obraz_1.jpg',
+    bezZdjecia: 'nophoto.png',
+    adres: ['Piękna 16', '40-003', 'Katowice', 'śląskie', 'Polska'],
+    plec: Plec.mezczyzna,
   };
   kolor: string = 'yellow';
   jak: string = 'center';
   aktywna: boolean = true;
+  pokazZdjecie: boolean = true;
   zmienKolor() {
     this.kolor = this.kolor === 'yellow' ? 'green' : 'yellow';
   }
--- src/app/interfaceOsoba.ts
+++ src/app/interfaceOsoba.ts
@@ -2,4 +2,13 @@ export interface Osoba {
   imie: string;
   nazwisko: string;
   zdjecie: string;
+  bezZdjecia: string;
+  adres: string[];
+  plec: Plec;
+}
+
+export enum Plec {
+  kobieta = 1,
+  mezczyzna,
+  niezdefiniowana,
 }
